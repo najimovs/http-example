7aac71a3b256cdab780c96cffa73eec4e2d67581
Muhammadxon Najimov <129692531+najimovs@users.noreply.github.com>
2025-12-14 17:43:57 +0500

    Create a basic server

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.info( "http-example" )
+import * as http from "node:http"
+
+const PORT = 3_000
+
+const server = http.createServer( ( req, res ) => {
+
+	res.writeHead( 200, { "Content-Type": "application/json" } )
+	res.end( JSON.stringify( { message: "OK" } ) )
+} )
+
+server.listen( PORT, "127.0.0.1", () => console.info( `:${ PORT }` ) )
